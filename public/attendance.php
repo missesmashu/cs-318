b5e875a533775473dbd9c6e604ad4709fd00d32c
missesmashu <hansontm@uwec.edu>
2023-09-05 15:48:00 -0500

    delete extra thing

--- public/attendance.php
+++
@@ -1,8 +0,0 @@
-<section>
-    <form action="https://getform.io/f/d4048e3e-86e3-4348-a2e1-9cba963ef660" method="POST">
-        <label>First Name<input type="text"></label>
-        <label>Last Name<input type="text"></label>
-        <label>Code<input type="text"></label>
-        <input type="submit">
-    </form>
-</section>
